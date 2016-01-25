9ceca60ce07c430d6a8c9b5e02eac020cb7f483d
Amir Ali Moinfar <moinfar.amirali@gmail.com>
2016-01-25 22:49:50 +0330

    Change semantic of attending teams in a game.
    base.Submit instances are now part of a game.

--- AIC_site/game/models.py
+++ AIC_site/game/models.py
@@ -21,7 +21,7 @@ class Game(models.Model):
     timestamp = models.DateTimeField(verbose_name=_('timestamp'), auto_now=True)
     competition = models.ForeignKey('game.Competition', verbose_name=_('competition'))
     title = models.CharField(verbose_name=_('title'), max_length=200)
-    players = models.ManyToManyField('base.Team', verbose_name=_('players'), through='game.GameTeam')
+    players = models.ManyToManyField('base.Submit', verbose_name=_('players'), through='game.GameTeamSubmit')
     config = models.FileField(verbose_name=_('config'))
 
     pre_games = models.ManyToManyField('game.Game', verbose_name=_('pre games'))
@@ -34,8 +34,8 @@ class Game(models.Model):
         verbose_name_plural = _('games')
 
 
-class GameTeam(models.Model):
-    team = models.ForeignKey('base.Team', verbose_name=_('team'))
+class GameTeamSubmit(models.Model):
+    submit = models.ForeignKey('base.Submit', verbose_name=_('team submit'))
     game = models.ForeignKey('game.Game', verbose_name=_('game'))
 
     score = models.IntegerField(verbose_name=_('score'))

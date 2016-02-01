c5f80fc88381098d84d5110eacb3896953e0a9e3
Amir Ali Moinfar <moinfar.amirali@gmail.com>
2016-02-01 13:09:57 +0330

    Fix some template issues.

--- 2016_template/templatetags/other_ssc_tags.py
+++ 2016_template/templatetags/other_ssc_tags.py
@@ -2,7 +2,7 @@ import locale
 import datetime
 from django import template
 from django.utils import timezone
-from django.forms.widgets import RadioSelect, CheckboxSelectMultiple
+from django.forms.widgets import RadioSelect, CheckboxSelectMultiple, FileInput, ClearableFileInput
 from .jdate_tags import farsi_digits
 register = template.Library()
 
@@ -18,6 +18,11 @@ def is_multiple_checkbox(instance):
 
 
 @register.filter()
+def is_file_input(instance):
+    return type(instance) == FileInput or type(instance) == ClearableFileInput
+
+
+@register.filter()
 def get_post_real_url(post):
     return post.get_absolute_url
 
@@ -26,6 +31,13 @@ def get_post_real_url(post):
 def post_is_new(post):
     return post.publish_date > timezone.now() - datetime.timedelta(days=7)
 
+
 @register.filter()
 def get_persian_comma_separated_money(number):
     return farsi_digits(format(number, ','))
+
+
+@register.filter()
+def log(object):
+    print(object)
+    return ''

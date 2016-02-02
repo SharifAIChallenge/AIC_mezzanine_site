c903ff8c97759b775017c0efd65ab33797858b44
Amir Ali Moinfar <moinfar.amirali@gmail.com>
2016-02-03 00:49:39 +0330

    Regenerate custom_pages mgrations.
    also fixed an unimportant bug!

--- custom_pages/migrations/0001_initial.py
+++ custom_pages/migrations/0001_initial.py
@@ -20,8 +20,13 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('_order', mezzanine.core.fields.OrderField(null=True, verbose_name='Order')),
                 ('creation_time', models.DateTimeField(auto_now_add=True, verbose_name='Creation Time')),
-                ('question', models.CharField(max_length=1024, verbose_name='Question')),
+                ('question', models.CharField(help_text='Ask New Question', max_length=1024, verbose_name='Question')),
+                ('question_fa', models.CharField(help_text='Ask New Question', max_length=1024, null=True, verbose_name='Question')),
+                ('question_en', models.CharField(help_text='Ask New Question', max_length=1024, null=True, verbose_name='Question')),
                 ('answer', models.CharField(max_length=4096, null=True, verbose_name='Answer', blank=True)),
+                ('answer_fa', models.CharField(max_length=4096, null=True, verbose_name='Answer', blank=True)),
+                ('answer_en', models.CharField(max_length=4096, null=True, verbose_name='Answer', blank=True)),
+                ('is_approved', models.BooleanField(default=True, verbose_name='Approved')),
             ],
             options={
                 'ordering': ('_order',),
@@ -46,6 +51,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='pages.Page')),
                 ('content', mezzanine.core.fields.RichTextField(verbose_name='Content')),
+                ('content_fa', mezzanine.core.fields.RichTextField(null=True, verbose_name='Content')),
+                ('content_en', mezzanine.core.fields.RichTextField(null=True, verbose_name='Content')),
                 ('responder_mail', models.EmailField(max_length=2014, null=True, verbose_name='Responder Mail', blank=True)),
             ],
             options={

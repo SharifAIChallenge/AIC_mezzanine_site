c7679b13b0522d507558feb4ac7f7d65c4fba10f
MJafar Mashhadi <raindigital2007@gmail.com>
2016-02-04 21:46:02 +0330

    Show invited members in panel

--- base/views.py
+++ base/views.py
@@ -143,12 +143,15 @@ def change_team_name(request, id):
 def my_team(request):
     team = request.team
     team_name_form = TeamNameForm(instance=team)
-    return render(request, 'custom/my_team.html', {'team': team, 'team_name_form': team_name_form})
+    invited_members = TeamInvitation.objects.filter(team=team, accepted=False).select_related('member').all()
+    return render(request, 'custom/my_team.html', {'team': team, 'team_name_form': team_name_form, 'invited_members': invited_members})
 
 
 @login_required
 @team_required
 def remove(request):
+    import json
+
     if request.method != 'POST':
         raise PermissionDenied()
     type = request.POST.get('type')
@@ -168,5 +171,37 @@ def remove(request):
             raise PermissionDenied()
         member.team = None
         member.save()
-    import json
+    elif type == 'invitation':
+        try:
+            invitation = TeamInvitation.objects.get(pk=id)
+        except TeamInvitation.DoesNotExist:
+            raise Http404()
+        is_head = request.team.head == request.user
+        if not is_head or request.team.pk != invitation.team.pk:
+            raise PermissionDenied()
+        invitation.delete()
+    else:
+        return HttpResponse(json.dumps({"success": False}), content_type='application/json')
+
     return HttpResponse(json.dumps({"success": True}), content_type='application/json')
+
+
+@login_required
+@team_required
+def resend_invitation_mail(request):
+    import json
+
+    if request.method != 'POST':
+        raise PermissionDenied()
+    id = request.POST.get('id')
+    try:
+        invitation = TeamInvitation.objects.get(pk=id)
+    except TeamInvitation.DoesNotExist:
+        raise Http404()
+    is_head = request.team.head == request.user
+    if not is_head or request.team.pk != invitation.team.pk:
+        raise PermissionDenied()
+
+    # TODO: naser , resend invitation mail
+    # TODO: Return localized message. Will show the message in UI.
+    return HttpResponse(json.dumps({"success": True, "message": ""}), content_type='application/json')

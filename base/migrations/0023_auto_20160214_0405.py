d94b448647f755b0f6f500ec35859e056bffdfd5
Hadi <h74629@gmail.com>
2016-02-14 00:36:04 +0000

    migrations

---
+++ base/migrations/0023_auto_20160214_0405.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0022_auto_20160214_0403'),
+    ]
+
+    operations = [
+    ]

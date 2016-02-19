eb66dedda535727847d1f55de71d1f8f36c6cf0a
Hadi <h74629@gmail.com>
2016-02-19 16:59:47 +0330

    Merge migrations.

---
+++ game/migrations/0024_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('game', '0023_auto_20160219_2020'),
+        ('game', '0023_game_error_log'),
+    ]
+
+    operations = [
+    ]

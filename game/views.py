9d6e436efe9dc03483bb7bfc79a90378ee32ab7b
Naser Aliabadi <aliabadi@arsh.co>
2016-02-19 21:08:31 +0330

    get scores as csv

--- game/views.py
+++ game/views.py
@@ -1,7 +1,8 @@
 # -*- coding:utf-8 -*-
 from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from game.forms import ScheduleForm
+from game.forms import ScheduleForm, TeamScoresForm
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -14,3 +15,15 @@ def schedule(request):
     else:
         form = ScheduleForm()
     return render(request, 'accounts/account_form.html', {'form': form, 'title': 'begin games'})
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def get_team_scores(request):
+    if request.method == 'POST':
+        form = TeamScoresForm(data=request.POST)
+        if form.is_valid():
+            csv_text = form.save()
+            return HttpResponse(csv_text, content_type='text/csv')
+    else:
+        form = TeamScoresForm()
+    return render(request, 'accounts/account_form.html', {'form': form, 'title': 'get scores'})

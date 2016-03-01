31a81486bbf4ffe875f3847f6c6e7c811ce0156a
Hadi <h74629@gmail.com>
2016-03-01 16:32:48 +0330

    Management command for setting teams money added

---
+++ base/management/commands/set_teams_money.py
@@ -0,0 +1,22 @@
+
+from django.core.management.base import BaseCommand
+from base.models import Team
+from django.db import transaction
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("teams_list_file", nargs=1, type=str)
+        parser.add_argument("payment_value", nargs=1, type=int)
+
+    def handle(self, *args, **options):
+        with open(options.get("teams_list_file")) as file:
+            with transaction.atomic():
+                for line in file.readlines():
+                    try:
+                        team = Team.objects.get(name=line)
+                        team.should_pay = True
+                        team.payment_value = options.get("payment_value")
+                        team.save()
+                    except:
+                        print("Problem in settiing team's money: %s" % line)
+                        raise Exception

cb1300ff39a689959bb623430034b250f6146b96
Naser Aliabadi <aliabadi@arsh.co>
2016-03-03 12:46:21 +0330

    fix

--- game/views.py
+++ game/views.py
@@ -113,8 +113,8 @@ def get_final_brackets(request):
         ],
         [
             [group.get_scores() for group in group_list[16:24]],
-            [group.get_scores() for group in group_list[40:32:-1]],
-            [group.get_scores() for group in group_list[44:40:-1]],
+            [group.get_scores() for group in group_list[39:31:-1]],
+            [group.get_scores() for group in group_list[43:39:-1]],
             [group_list[49].get_scores(), group_list[48].get_scores(), group_list[51].get_scores(),
              group_list[50].get_scores()],
             [group_list[52].get_scores(), group_list[53].get_scores()],

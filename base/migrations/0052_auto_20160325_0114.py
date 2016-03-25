65504454b91ce39ce0c54c9a07d02c8fc0da03ee
Iman <iman.jami@gmail.com>
2016-03-26 00:55:41 +0430

    staffs page

---
+++ base/migrations/0052_auto_20160325_0114.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import base.models
+import django.core.files.storage
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0051_auto_20160318_2000'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='staffmember',
+            name='image',
+            field=models.ImageField(storage=django.core.files.storage.FileSystemStorage(), upload_to=b'staff/images/', null=True, verbose_name='image'),
+        ),
+        migrations.AlterField(
+            model_name='staffteam',
+            name='icon',
+            field=models.ImageField(storage=django.core.files.storage.FileSystemStorage(), upload_to=b'staff/teams/icons/', null=True, verbose_name='icon'),
+        ),
+        migrations.AlterField(
+            model_name='submit',
+            name='code',
+            field=models.FileField(upload_to=base.models.team_code_directory_path, storage=django.core.files.storage.FileSystemStorage(), verbose_name='code'),
+        ),
+        migrations.AlterField(
+            model_name='submit',
+            name='compiled_code',
+            field=models.FileField(storage=django.core.files.storage.FileSystemStorage(), upload_to=base.models.team_compiled_code_directory_path, null=True, verbose_name='compiled code', blank=True),
+        ),
+    ]

0af2c2705b5a24d4a8f6f3a08a2f90ccc1f0c089
somagh <aryakowsary@yahoo.com>
2017-02-14 21:08:24 +0330

    cleaning

--- base/tasks.py
+++ base/tasks.py
@@ -16,7 +16,6 @@ def get_reports():
     client = coreapi.Client(transports=transports)
     schema = client.get(settings.BASE_MIDDLE_BRAIN_API_SCHEMA)
     reports=client.action(schema,['run','report','list'],params={'time':0})
-    print(reports)
     for report in reports:
         if(len(Submit.objects.filter(run_id=report['id']))>0):
             submit=Submit.objects.get(run_id=report['id'])
@@ -25,9 +24,6 @@ def get_reports():
             if(submit.status!=3):
                 log = json.loads(client.action(schema, ['storage', 'get_file', 'read'],
                 params={'token': report['parameters']['code_log']}).read())
-                print(log)
-                if log is None:
-                    log = {"errors": []}
                 if len(log["errors"]) == 0:
                     submit.status = 3
                 else:

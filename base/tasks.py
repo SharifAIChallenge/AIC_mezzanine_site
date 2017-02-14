5839544d8f3f49e7029d818d3967cbd0a9264aca
somagh <aryakowsary@yahoo.com>
2017-02-14 20:43:52 +0330

    get-file action returns file

--- base/tasks.py
+++ base/tasks.py
@@ -3,31 +3,10 @@
 import os
 
 import coreapi
-import celery
-from celery import Celery
 from celery import shared_task
-from celery.schedules import crontab
-from celery.task import periodic_task
-# from django.conf import settings
-#from AIC_site import _celery
-# #from AIC_SITE._celery import app
-#
-# from base.models import Submit
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AIC_site.settings')
-# app = Celery('AIC')
-# app.config_from_object('django.conf:settings')
-# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-
-
-
-#@periodic_task(run_every=crontab(hour="*", minute="0", day_of_week="*"), ignore_result=True)
-#from base.models import Submit
 from django.conf import settings
-
 from base.models import Submit
+import json
 
 
 @shared_task
@@ -39,17 +18,16 @@ def get_reports():
     reports=client.action(schema,['run','report','list'],params={'time':0})
     print(reports)
     for report in reports:
-        submit=Submit.objects.get(run_id=report['id'])
-
-
+        if(len(Submit.objects.filter(run_id=report['id']))>0):
+            submit=Submit.objects.get(run_id=report['id'])
         if(report['status']==2):
             submit.compiled_id=report['parameters']['code_compiled_zip']
             if(submit.status!=3):
-                log = client.action(schema, ['storage', 'get_file', 'read'],
-                params={'token': report['parameters']['code_log']})
+                log = json.loads(client.action(schema, ['storage', 'get_file', 'read'],
+                params={'token': report['parameters']['code_log']}).read())
+                print(log)
                 if log is None:
                     log = {"errors": []}
-                print(log)
                 if len(log["errors"]) == 0:
                     submit.status = 3
                 else:

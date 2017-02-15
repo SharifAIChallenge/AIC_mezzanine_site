81c2f5cd5d561b1bf9be55f4d0f2b83aeea9ee74
somagh <aryakowsary@yahoo.com>
2017-02-15 23:04:06 +0330

    fix

--- base/tasks.py
+++ base/tasks.py
@@ -16,14 +16,14 @@ def get_reports():
     transports = [coreapi.transports.HTTPTransport(credentials=credientals)]
     client = coreapi.Client(transports=transports)
     schema = client.get(settings.BASE_MIDDLE_BRAIN_API_SCHEMA)
-    reports=client.action(schema,['run','report','list'],params={'time':int(LastGetReportsTime.get_solo().time)-30})
+    reports=client.action(schema,['run','report','list'],params={'time':int(LastGetReportsTime.get_solo().time)-10})
     for report in reports:
         if(len(Submit.objects.filter(run_id=report['id']))==0):
             continue
         submit=Submit.objects.get(run_id=report['id'])
         if(report['status']==2):
             submit.compiled_id=report['parameters']['code_compiled_zip']
-            if(submit.status!=3):
+            if(submit.status<3):
                 logfile=client.action(schema, ['storage', 'get_file','read'],
                 params={'token': report['parameters']['code_log']})
                 if(logfile is None):
